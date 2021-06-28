f8161d3b525b1cbc0376e1e47f6f9b25a5ae402d
Anas Mohamed <anaskamal659@gmail.com>
2021-06-27 20:13:36 -0700

    add note unit test

---
+++ KoinzTaskTests/AddNoteTest.swift
@@ -0,0 +1,42 @@
+//
+//  AddNoteTest.swift
+//  KoinzTaskTests
+//
+//  Created by no one on 27/06/2021.
+//
+
+import XCTest
+@testable import KoinzTask
+class AddNoteTest: XCTestCase {
+
+    var noteDetailsViewModel : NoteDetailsViewModel!
+    var note = Note()
+    override func setUp() {
+        noteDetailsViewModel = NoteDetailsViewModel()
+        note.title = "title"
+        note.details = "details"
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    func testAddNote()  {
+        DatabaseManager.shared.add(note: note)
+        XCTAssertNotNil(DatabaseManager.shared.fetchNotes())
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
